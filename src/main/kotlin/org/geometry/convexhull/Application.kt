7af6379e1e3c6643c5172b936fcd6cecb0b6a880
antonarhipov <ant.arhipov@gmail.com>
2025-03-27 00:42:05 +0100

    blank lines

--- src/main/kotlin/org/geometry/convexhull/Application.kt
+++ src/main/kotlin/org/geometry/convexhull/Application.kt
@@ -1,15 +1,21 @@
 package org.geometry.convexhull
 
 import com.apple.eawt.Application
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 class MyClass {
     fun someFunction() {}
 }
 
+@SpringBootApplication
+class Application
+
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
+
+// split list of elements into a list of list by example element
 
 
 
