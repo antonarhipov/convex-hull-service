0b090dcacd394600269c212c05ba9e619d28cc6c
antonarhipov <ant.arhipov@gmail.com>
2025-07-10 16:45:06 +0300

    Refine prompt formatting and improve clarity in documentation.

--- src/main/kotlin/org/geometry/convexhull/Application.kt
+++ src/main/kotlin/org/geometry/convexhull/Application.kt
@@ -1,12 +1,7 @@
 package org.geometry.convexhull
 
-import com.apple.eawt.Application
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
-class MyClass {
-    fun someFunction() {}
-}
 
 @SpringBootApplication
 class Application
