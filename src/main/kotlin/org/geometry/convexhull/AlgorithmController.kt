35b4b3b546408eb6b626c7a017a0043d47e2af9a
antonarhipov <ant.arhipov@gmail.com>
2025-08-05 16:09:41 +0300

    initial state

--- src/main/kotlin/org/geometry/convexhull/AlgorithmController.kt
+++ src/main/kotlin/org/geometry/convexhull/AlgorithmController.kt
@@ -2,6 +2,8 @@ package org.geometry.convexhull
 
 import org.geometry.convexhull.ext.Coordinate
 import org.geometry.convexhull.ext.JarvisMarch
+import org.geometry.math.D
+import org.geometry.math.TheAlgorithm
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -11,9 +13,9 @@ import kotlin.collections.toTypedArray
 @RestController
 class AlgorithmController {
     @PostMapping("/ds")
-    fun createDList(@RequestBody ds: List<Coordinate>): ResponseEntity<List<Coordinate>> {
+    fun createDList(@RequestBody ds: List<D>): ResponseEntity<List<D>> {
         val input = ds.toTypedArray()
-        val result = JarvisMarch().run(ds)
+        val result = TheAlgorithm.run(input)
         println("The convex contains " + result.size + " points")
         return ResponseEntity.ok(result)
     }
